0bd6a7d191077ce19b083f5dd492daf262fe9b35
SHREEHARI285 <shreeharipappaka@gmail.com>
2026-06-09 11:59:21 +0530

    fixed productivity  module SQLAlchemy conflics

--- model/Productivity/report_log.py
+++ model/Productivity/report_log.py
@@ -1,16 +1,14 @@
-# app/models/report_log.py
-from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey,func
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from core.database import Base
-# ReportLog Model
+
 class ReportLog(Base):
     __tablename__ = "report_logs"
 
     id = Column(Integer, primary_key=True, index=True)
-    report_type = Column(String(100), nullable=False)
     generated_by = Column(Integer, ForeignKey("employees.id", ondelete="SET NULL"), nullable=True)
-    file_path = Column(String, nullable=False)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    report_type = Column(String(100), nullable=True)
+    generated_at = Column(DateTime(timezone=True), server_default=func.now())
 
-    generator = relationship("Employee", backref="generated_reports", foreign_keys=[generated_by])
+    generator = relationship("Employee", foreign_keys=[generated_by])
